3c24902b539a9ea092399a0210b29186a07ebf56
BrendanOssebaar <90763311+BrendanOssebaar@users.noreply.github.com>
2024-01-29 09:50:58 +0100

    Update SelectCommand.cs

--- RTS/Assets/Scripts/SelectCommand.cs
+++ RTS/Assets/Scripts/SelectCommand.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 public class SelectCommand : Command
 {
     private bool isCompleted;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public override bool Execute(GameObject entity)
     {
         SelectEntity();
